8d6ef6668994ebee560dee6e8423a11baa4cae73
Carlos Tosta <cectt@yahoo.com.br>
2020-11-03 18:28:23 -0300

    AULA 1 - ARRAY E DICIONARIO

--- js/mais.js
+++ js/mais.js
@@ -10,3 +10,22 @@ console.log(idade + idade2);
 console.log(frase.toLocaleUpperCase());
 console.log(frase.toLowerCase());
 console.log(n1 * n2);
+
+var lista = ["maçã", "pêra", "laranja"]; // variavel tipo array
+//lista.push("uva"); //adiciona uva na lista
+//lista.pop(""); ///remove o último elemento da lista
+console.log(lista);
+console.log(lista[1]);
+console.log(lista.length); //exibve o tamanho da lista
+console.log(lista.reverse); //mostra os elementos de forma invertida
+console.log(lista.toString); //transforma os elementos em string
+console.log(lista.join(" - ")) //transforma os elementos em string e separa com traços
+alert(lista[1]); //exibe um alerta com o elemento 1 da lista
+
+var fruta = { nome: "maçã", cor: "vermelha" }; //variavel tipo dicionário
+console.log(fruta.nome);
+alert(fruta.cor);
+
+var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]; //variavel tipo dicionários
+console.log(frutas[1].nome);
+alert(frutas.cor);
