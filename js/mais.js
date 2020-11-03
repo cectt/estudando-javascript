4847def42b93090cae26bcc9ebc26591309946af
Carlos Tosta <cectt@yahoo.com.br>
2020-11-03 12:04:46 -0300

    AULA 1 - INTRODUÇÃO AO JAVASCRIPT

---
+++ js/mais.js
@@ -0,0 +1,12 @@
+var nome = "Carlos Tosta";
+var idade = 42;
+var idade2 = 10;
+var n1 = 5;
+var n2 = 3;
+var frase = "Brasil é o melhor time do mundo"
+alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLocaleUpperCase());
+console.log(frase.toLowerCase());
+console.log(n1 * n2);
